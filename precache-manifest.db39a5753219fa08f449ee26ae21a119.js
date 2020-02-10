622608d3a411b0969af4bc5fb27671c31b9bff74
Juan Garcia <jgarcia1545@gmail.com>
2020-02-09 20:53:18 -0800

    Updates

---
+++ precache-manifest.db39a5753219fa08f449ee26ae21a119.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb5d3238109fe54c167d01c697f67969",
+    "url": "/Songs/index.html"
+  },
+  {
+    "revision": "f9415d02214066d48e30",
+    "url": "/Songs/static/js/2.ad7b6732.chunk.js"
+  },
+  {
+    "revision": "3d49cc1f8b8239fa5d9c",
+    "url": "/Songs/static/js/main.1eb3f7c4.chunk.js"
+  },
+  {
+    "revision": "3aa942cb331cc238404e",
+    "url": "/Songs/static/js/runtime-main.79681f7d.js"
+  }
+]);
